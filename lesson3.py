9b992f010681e4c8418d226b9913f6f174f37055
Mehmet Ali Işık <isikmehmetali0123@gmail.com>
2025-10-03 20:21:02 +0300

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,10 @@
+#Assigning values ​​to different variables in one line
+x,y,z="Apple","Banana","Grape"
+
+print(x)
+print(y)
+print(z)
+
+name="Steven"
+lastname=name 
+print(lastname)
---
+++ lesson5.py
@@ -0,0 +1,9 @@
+#Why variables
+number=7
+print(number)
+
+number=21
+print(number)
+
+number=number*2
+print(number)
